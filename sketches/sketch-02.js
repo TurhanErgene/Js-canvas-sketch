abc38693bbf5cf6a61709521b9b0d584ed53ef14
Turhan <turhan-ergene@hotmail.com>
2022-01-30 01:43:03 +0100

    sketch-02 clock

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -4,6 +4,10 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
+const degToRad = (degrees) => {
+  return degrees / 180 * Math.PI;
+}
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -11,17 +15,44 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
 
-    context.translate(x, y);
-    context.rotate(0.75)
+    const w = width * 0.01;
+    const h = height * 0.1;
+    let x, y;
+
+    const num = 12;
+    const radius = width * 0.3
+
+    for (let i = 0; i < num; i++) {
+
+      const slice = degToRad(360 / num)
+      const angle = slice * i;
+
+      x = cx + radius * Math.sin(angle)
+      y = cy + radius * Math.cos(angle)
+
+      context.save()
+      context.translate(x, y);
+      context.rotate(-angle)//*4
+      
+      context.beginPath();
+      context.rect(-w / 2, -h /2, w, h);
+      context.fill();
+      context.restore();
+
+    }
     
-    context.beginPath();
-    context.rect(-w / 2, -h /2, w, h);
-    context.fill();
+    
+
+    
+    // context.translate(100, 400);
+    
+    // context.beginPath();
+    // context.arc(0, 0, 50, 0, MATH.PI * 2)
+    // context.fill();
+
   };
 };
 
